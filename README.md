83a8593ba6334628bb677836a316aafbdd26c905
notjawad404 <jawadali000123@gmail.com>
2023-07-03 00:06:11 +0500

    Added Readme File

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Flutter-Bootcamp
+
+<ol>
+    <li> Added Flutter-Application1 </li>
+    <li> Added Dart-task branch </li>
+
+</ol>
--- q1.dart
+++
@@ -1,7 +0,0 @@
-void main() {
-  List<double> gpaList = [2.63, 2.79, 2.33, 2.74, 3.35, 3.36];
-
-  double listSum = gpaList.reduce((value, element) => value + element);
-  double cgpa = listSum / gpaList.length;
-  print('CGPA = $cgpa');
-}
